3fba78c4a5f19e1f7b84de47ee6d86732275143b
Saeed <saeedshahib@gmail.com>
2022-12-30 02:25:19 +0330

    repay loan added

--- medical/admin.py
+++ medical/admin.py
@@ -15,4 +15,10 @@ class MedicalLoanFormAdmin(BaseAdmin):
     search_fields = ['phone_number', 'email']
 
 
+class LoanRepayEventAdmin(BaseAdmin):
+    list_display = [field.name for field in medical_models.LoanRepayEvent._meta.fields]
+    search_fields = ['checkout_request_id']
+
+
 admin.site.register(medical_models.MedicalLoanForm, MedicalLoanFormAdmin)
+admin.site.register(medical_models.LoanRepayEvent, LoanRepayEventAdmin)
