93ab27a71b6efdf633051abd2b62f9a5daf8ea23
Saeed <saeedshahib@gmail.com>
2023-01-06 01:07:45 +0330

    mobile payment with IPay added

--- medical/views.py
+++ medical/views.py
@@ -8,6 +8,7 @@ from medical import models as medical_models
 from django.core.exceptions import ValidationError
 
 from medical.exceptions import BaseExceptionManager
+from medical.ipay_api import IPay
 
 
 # Create your views here.
@@ -64,6 +65,59 @@ class RepayLoanView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         except medical_models.MedicalLoanForm.DoesNotExist as ve:
             return Response({'error': 'This medical loan form does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+        except BaseExceptionManager as ve:
+            return Response({'error': ve.message}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as ve:
+            print(ve)
+            return Response({'error': 'Server error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class MobileRepayLoanView(APIView):
+    @staticmethod
+    def post(request):
+        medical_loan_id = request.data.get('medical_loan_id')
+        email = request.data.get('email')
+        phone_number = request.data.get('phone_number')
+        months_to_repay = int(request.data.get('months_to_repay'))
+
+        try:
+            medical_loan_form = medical_models.MedicalLoanForm.objects.get(id=int(medical_loan_id), email=email,
+                                                                           phone_number=phone_number)
+            data = medical_loan_form.request_payment_with_ipay(months_count=months_to_repay)
+
+            return Response(data, status=status.HTTP_201_CREATED)
+        except medical_models.MedicalLoanForm.DoesNotExist as ve:
+            return Response({'error': 'This medical loan form does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+        except BaseExceptionManager as ve:
+            return Response({'error': ve.message}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as ve:
+            print(ve)
+            return Response({'error': 'Server error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @staticmethod
+    def get(request):
+        order_id = request.GET.get('id')
+        status = request.GET.get('status')
+        ivm = request.GET.get('ivm')
+        qwh = request.GET.get('qwh')
+        afd = request.GET.get('afd')
+        poi = request.GET.get('poi')
+        uyt = request.GET.get('uyt')
+        ifd = request.GET.get('ifd')
+
+        try:
+            loan_repay_event = medical_models.LoanRepayEvent.objects.get(checkout_request_id=order_id)
+        except medical_models.LoanRepayEvent.DoesNotExist:
+            return Response({'error': 'This payment does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if status != "aei7p7yrx4ae34":
+            return Response({"error": "payment failed"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            loan_repay_event.verify_ipay_payment(order_id=order_id, ivm=ivm, qwh=qwh, afd=afd, poi=poi, uyt=uyt, ifd=ifd)
+            loan_repay_event.refresh_from_db()
+            serializer = medical_serializers.MedicalLoanSerializers(loan_repay_event.medical_loan_form)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         except BaseExceptionManager as ve:
             return Response({'error': ve.message}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as ve:
